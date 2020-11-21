828c79a4cb4bfe9bb92acc802eb656d0d8b8062f
nojaf <florian.verdonck@outlook.com>
2020-11-21 14:14:17 +0100

    Updates

--- _dist_/FantomasTools/Http.fs
+++
@@ -1,8 +0,0 @@
-module FantomasTools.Client.Http
-
-open Fable.Core
-open Fable.Core.JsInterop
-
-let postJson<'TResponse> _url _body: JS.Promise<int * 'TResponse> = import "postJson" "../js/http"
-
-let getText _url: JS.Promise<string> = import "getJson" "../js/http"
--- _dist_/FantomasTools/VersionBar.fs
+++
@@ -1,9 +0,0 @@
-module FantomasTools.Client.VersionBar
-
-open Fable.React
-open Fable.React.Props
-
-let versionBar version =
-    div [ ClassName "version-bar" ] [
-        str version
-    ]
