cdd4c0ef7557abbde38c1d54671e6e758145b12b
nojaf <florian.verdonck@outlook.com>
2020-11-21 17:11:42 +0100

    Updates

--- _dist_/bin/bin/.fable/Thoth.Json.5.0.0/Extra.fs.js
+++
@@ -1,5 +0,0 @@
-import { empty as empty_1 } from "../../../.fable/fable-library.3.0.0-nagareyama-rc-008/Map.js";
-import { ExtraCoders } from "./Types.fs.js";
-
-export const empty = new ExtraCoders("", empty_1());
-
