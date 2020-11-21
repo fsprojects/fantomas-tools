ab8528a1e6df0b0922a103a7f519771378bb5da2
nojaf <florian.verdonck@outlook.com>
2020-11-21 18:03:39 +0100

    Updates

--- _dist_/bin/Http.js
+++
@@ -1,6 +0,0 @@
-import { getJson, postJson as postJson_1 } from "../js/http.js";
-
-export const postJson = postJson_1;
-
-export const getText = getJson;
-
--- web_modules/prop-types.js
+++
@@ -1,3 +0,0 @@
-import './common/_commonjsHelpers-8c19dec8.js';
-import { p as propTypes } from './common/index-ce016b4a.js';
-export { p as default } from './common/index-ce016b4a.js';
