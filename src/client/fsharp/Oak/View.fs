f03328f943fa1469ad8ef8d8d4c4daff4027b9b6
jindraivanek <jindra.ivanek@gmail.com>
2023-01-09 10:48:35 +0100

    memoize utils moved to separate file

--- src/client/fsharp/Oak/View.fs
+++ src/client/fsharp/Oak/View.fs
@@ -9,10 +9,10 @@ open Fable.Core.JsInterop
 open Fable.React
 open Fable.React.Props
 open FantomasTools.Client
+open FantomasTools.Client.Utils
 open FantomasTools.Client.OakViewer.Model
 open FantomasTools.Client.OakViewer.Graph
 open FantomasTools.Client.OakViewer.Model.GraphView
-open Feliz
 open Reactstrap
 
 type NodeType =
@@ -30,24 +30,6 @@ type OakNode = {
     Childs: OakNode list
     Limited: bool
 }
-
-let memoizeBy (g: 'a -> 'c) (f: 'a -> 'b) =
-    let cache = System.Collections.Generic.Dictionary<_, _>()
-
-    fun x ->
-        let key = g x
-
-        if cache.ContainsKey key then
-            cache[key]
-        else
-            let y = f x
-            cache.Add(key, y)
-            y
-
-let inline memoize f = memoizeBy id f
-
-let inline memoize2 f =
-    memoizeBy id (fun (x, y) -> f x y) |> fun f -> fun x y -> f (x, y)
 
 let nodesFromRoot root =
     let rec getChilds acc n =
