b4c559beb19e6c22ac66b5032a752d96773306d8
Florian Verdonck <florian.verdonck@outlook.com>
2024-02-14 12:47:29 +0000

    Use vite-plugin-fable (#632)
    * Use vite-plugin-fable
    
    * Add dev script
    
    * Resolve fsproj safer
    
    * bump plugin
    
    * Update vite-plugin-fable to 0.0.9
    
    * 0.0.10

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,19 +1,20 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fable from 'vite-plugin-fable';
+
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+const fsproj = path.join(currentDir, 'fsharp/FantomasTools.fsproj');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'classic' })],
+  plugins: [react({ jsxRuntime: 'classic' }), fable({ fsproj })],
   server: {
     port: 9060,
-    watch: {
-      ignored: [
-        '**/*.fs',
-        '**/*.fsi', // Don't watch F# files
-      ],
-    },
   },
   build: {
     outDir: 'build',
   },
+  base: '/fantomas-tools/',
 });
